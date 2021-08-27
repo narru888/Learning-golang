de6e42995bc14b859d810f483a20572cf824a16f
dengnarru <you@example.com>
2021-08-28 00:27:29 +0800

    first

--- template.go
+++ template.go
@@ -1,15 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+// 脚本目的：
+// xxxxxxxxxxxxxxxxxxxxxxxx
+//
+// 实现功能：
+// 使用XXXXX组件，实现了XXXXXX功能
+//
 // 参考连接：http://www.topgoer.com/beego%E6%A1%86%E6%9E%B6/beego%E7%9A%84mvc%E6%9E%B6%E6%9E%84%E4%BB%8B%E7%BB%8D/model%E8%AE%BE%E8%AE%A1/orm%E4%BD%BF%E7%94%A8.html
 
-// 运行前提
-//
-// 1. 
-//
+
+// 运行前提:
+// 1. XXXXX
+// 2. xxxxxx
 //
 
 
@@ -18,10 +22,9 @@ import (
 //
 
 // 用例1.1
-//
-
-func addSQL()  {
-	fmt.Println("addSQL")
+func addSQL(param int)  int{
+	fmt.Print("running Func addSQL ...")
+	return param
 }
 
 //
@@ -54,5 +57,6 @@ func addSQL()  {
 
 
 func main() {
-	addSQL()
+	var num int // 默认值为0（不赋值）。 普通赋值方法 ps：var num int =1
+	addSQL(num)
 }
